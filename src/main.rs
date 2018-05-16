bb254752c5d6b61ac47472be33584261ca61374d
yochidrop <miyazawa@nagisa-inc.jp>
2018-05-16 16:05:54 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 #[macro_use]
 extern crate serde_derive;
 
-extern crate serde;
-extern crate serde_json;
-
 mod models;
 mod scraper;
 mod serializer;
@@ -25,6 +22,6 @@ fn main() {
     let selected_langs: Vec<String> = scraper::select_languages(&mut response);
     let data = models::country::CountryData::create(&selected_langs);
     let json_serialize = serializer::serialize_from_countries(&data);
-    
+
     println!("{}", json_serialize);
 }
